4dee17533feedf9e57eeac43719eff68ff345a54
fitzypop <fitzypop@pm.me>
2022-03-30 15:00:15 -0600

    small modiications

--- immute.py
+++ immute.py
@@ -1,37 +1,36 @@
 import inspect
-from typing import Any
 
 
 class ImmutableMeta(type):
-    def __delattr__(self, __name: str) -> None:
+    def __delattr__(self, *args) -> None:
         raise TypeError(
-            f"Objects of type '{type(self).__name__}' are immutable, "
+            f"Objects derived from '{type(self).__name__}' are immutable, "
             "and do not support deletions."
         )
 
-    def __setattr__(self, __name: str, __value: Any) -> None:
+    def __setattr__(self, *args) -> None:
         print(inspect.stack()[1][3])
         if inspect.stack()[1][3] in ("__init__", "__new__"):
-            object.__setattr__(self, __name, __value)
+            object.__setattr__(self, *args)
         else:
             raise TypeError(
                 f"Objects of type '{type(self).__name__}' are immutable, "
-                "and do not support assignments."
+                "and do not support assignments or reassignment operations."
             )
 
 
 class Immutable(metaclass=ImmutableMeta):
-    def __delattr__(self, __name: str) -> None:
+    def __delattr__(self, *args) -> None:
         raise TypeError(
             f"Objects of type '{type(self).__name__}' are immutable, "
             "and do not support deletions."
         )
 
-    def __setattr__(self, __name: str, __value: Any) -> None:
+    def __setattr__(self, *args) -> None:
         if inspect.stack()[1][3] == "__init__":
-            object.__setattr__(self, __name, __value)
+            object.__setattr__(self, *args)
         else:
             raise TypeError(
                 f"Objects of type '{type(self).__name__}' are immutable, "
-                "and do not support assignments."
+                "and do not support assignments or reassignment operations."
             )
