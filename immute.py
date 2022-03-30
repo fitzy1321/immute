9412211b20d1487d3411f8a598bbd0e8b2908772
fitzypop <fitzypop@pm.me>
2022-03-30 15:46:36 -0600

    removed duplicate code by moving logic to functions called from classes

--- immute.py
+++ immute.py
@@ -1,35 +1,40 @@
 import inspect
 
+_SAFE_MUT_FUNCS = ("__init__", "__new__")
+
+
+def _delattr(self) -> None:
+    raise TypeError(
+        f"Objects of type '{type(self).__name__}' are immutable, "
+        "and do not support assignments or reassignment operations."
+    )
+
+
+def _setattr(self, *args) -> None:
+    # Use inspect to traverse function stacks
+    # First func should be __settatr__
+    # Second func should be init or new
+    prev_func = inspect.stack()[2][3]
+    if prev_func in _SAFE_MUT_FUNCS:
+        object.__setattr__(self, *args)
+    else:
+        raise TypeError(
+            f"Objects of type '{type(self).__name__}' are immutable, "
+            "and do not support assignments or reassignment operations."
+        )
+
 
 class ImmutableMeta(type):
     def __delattr__(self, *args) -> None:
-        raise TypeError(
-            f"Objects derived from '{type(self).__name__}' are immutable, "
-            "and do not support deletions."
-        )
+        _delattr(self)
 
     def __setattr__(self, *args) -> None:
-        if inspect.stack()[1][3] in ("__init__", "__new__"):
-            object.__setattr__(self, *args)
-        else:
-            raise TypeError(
-                f"Objects of type '{type(self).__name__}' are immutable, "
-                "and do not support assignments or reassignment operations."
-            )
+        _setattr(self, *args)
 
 
 class Immutable(metaclass=ImmutableMeta):
     def __delattr__(self, *args) -> None:
-        raise TypeError(
-            f"Objects of type '{type(self).__name__}' are immutable, "
-            "and do not support deletions."
-        )
+        _delattr(self)
 
     def __setattr__(self, *args) -> None:
-        if inspect.stack()[1][3] in ("__init__", "__new__"):
-            object.__setattr__(self, *args)
-        else:
-            raise TypeError(
-                f"Objects of type '{type(self).__name__}' are immutable, "
-                "and do not support assignments or reassignment operations."
-            )
+        _setattr(self, *args)
--- tests/immutable_test.py
+++ tests/immutable_test.py
@@ -1,10 +1,19 @@
 import pytest
 from immute import Immutable
 
+NUM = 42
+
 
 class Thing(Immutable):
+    class_var = "Does this work?"
+
     def __init__(self) -> None:
-        self.num = 42
+        self.num = NUM
+
+
+def test_class_init_still_works():
+    thing = Thing()
+    assert thing.num == NUM
 
 
 def test_reassignment_raises_type_error():
@@ -25,6 +34,19 @@ def test_del_raises_type_error():
         del thing.num
 
 
-def test_assignmnet_to_class_raises_type_error():
-    with pytest.raises(TypeError) as e:
+def test_class_variable_assignmnet_outside_class_def_raises_type_error():
+    with pytest.raises(TypeError):
         Thing.m_str = "Something ..."
+
+
+def test_class_variable_assignment_in_class_def_still_works():
+    try:
+        if Thing.class_var:
+            assert True
+    except Exception:
+        assert False
+
+
+def test_class_variable_del_raises_type_error():
+    with pytest.raises(TypeError):
+        del Thing.class_var
